8d4e6324bd9340ea7af3d3ebd6d0b506eac994d4
writetofahim <writetofahimahammad@gmail.com>
2023-09-11 11:40:46 +0600

    ssl added to instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8800/api/",
+  baseURL: "https://127.0.0.1:8800/api/",
   // baseURL: "https://aksesdpbackend.onrender.com/api",
   // baseURL: "http://aksesdpmhs.itstorm.xyz/api",
 });
