c82105f852885e60f68866738b331653a891ac56
writetofahim <writetofahimahammad@gmail.com>
2023-09-11 11:56:07 +0600

    ssl added to right instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://127.0.0.1:8800/api/",
+  // baseURL: "http://127.0.0.1:8800/api/",
   // baseURL: "https://aksesdpbackend.onrender.com/api",
-  // baseURL: "http://aksesdpmhs.itstorm.xyz/api",
+  baseURL: "https://aksesdpmhs.itstorm.xyz/api",
 });
 export default axiosInstance;
