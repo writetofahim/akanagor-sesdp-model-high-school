f2ddf9e12c556554821858efea9678b54cff4b29
writetofahim <writetofahimahammad@gmail.com>
2023-09-25 13:47:30 +0600

    api url changed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:8800/api",
   // baseURL: "https://aksesdpbackend.onrender.com/api",
-  baseURL: "https://aksesdpmhs.itstorm.xyz/api",
+  // baseURL: "https://aksesdpmhs.itstorm.xyz/api",
+  baseURL: "https://aksesdpmhs.fahimahammad.info/api",
 });
 export default axiosInstance;
